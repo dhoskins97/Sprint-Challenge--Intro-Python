41764788a885b3378122ee7a0084cc2e7b47d683
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-12-06 12:54:31 -0500

    bugfix

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -6,6 +6,8 @@ class City:
     self.name = name
     self.lat = lat
     self.lon = lon
+  def __repr__(self):
+    return f"City(\"{self.name}\", {self.lat}, {self.lon})"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -36,7 +38,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(f"{c.name}: {c.lat}, {c.lon}.")
+    print(c)
 
 # STRETCH GOAL!
 #
